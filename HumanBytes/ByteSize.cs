1f83abe05e4272a09b0e5d344862b1e6f8d5cdad
Thomas Levesque <tom103@free.fr>
2014-08-19 22:44:38 +0200

    Completed documentation; created NuGet package

--- HumanBytes/ByteSize.cs
+++ HumanBytes/ByteSize.cs
@@ -1,74 +1,189 @@
-﻿namespace HumanBytes
+﻿using System;
+
+namespace HumanBytes
 {
+    /// <summary>
+    /// Represents a size in bytes.
+    /// </summary>
     public struct ByteSize
     {
         private readonly long _value;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ByteSize"/> struct.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
         public ByteSize(long value)
         {
             _value = value;
         }
 
+        /// <summary>
+        /// Gets the size in bytes.
+        /// </summary>
+        /// <value>
+        /// The size in bytes.
+        /// </value>
         public long Value
         {
             get { return _value; }
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        /// <remarks>
+        /// The size is formatted to a human readable format using the default formatter (<see cref="ByteSizeFormatter.Default"/>).
+        /// </remarks>
         public override string ToString()
         {
             return ByteSizeFormatter.Default.Format(_value);
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance, using the specified formatter.
+        /// </summary>
+        /// <param name="formatter">The formatter to use.</param>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException"><c>formatter</c> is null.</exception>
+        public string ToString(ByteSizeFormatter formatter)
+        {
+            if (formatter == null) throw new ArgumentNullException("formatter");
+            return formatter.Format(_value);
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Int64"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(long value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.UInt64"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(ulong value)
         {
             return new ByteSize(checked((long)value));
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Int32"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(int value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.UInt32"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(uint value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Int16"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(short value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.UInt16"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(ushort value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Byte"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(byte value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.SByte"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(sbyte value)
         {
             return new ByteSize(value);
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Double"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(double value)
         {
             return new ByteSize(checked((long) value));
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Single"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(float value)
         {
             return new ByteSize(checked((long) value));
         }
 
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="System.Decimal"/> to <see cref="ByteSize"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
         public static implicit operator ByteSize(decimal value)
         {
             return new ByteSize(checked((long) value));
--- HumanBytes/ByteSizeExtensions.cs
+++ HumanBytes/ByteSizeExtensions.cs
@@ -1,57 +1,115 @@
 ﻿namespace HumanBytes
 {
+    /// <summary>
+    /// Provide extension methods to convert numeric values to instances of <see cref="ByteSize"/>.
+    /// </summary>
     public static class ByteSizeExtensions
     {
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this long value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this ulong value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this int value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this uint value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this short value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this ushort value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this byte value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this sbyte value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this double value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this float value)
         {
             return value;
         }
 
+        /// <summary>
+        /// Returns an instance of <see cref="ByteSize"/> that represents the specified size.
+        /// </summary>
+        /// <param name="value">The size in bytes.</param>
+        /// <returns>An instance of <see cref="ByteSize"/> that represents the specified size.</returns>
         public static ByteSize Bytes(this decimal value)
         {
             return value;
