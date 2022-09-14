cd83c9e7b67bc496289a17e0f3d84226038ac851
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2022-09-13 23:10:06 -0400

    Implement IEquatable and IComparable

--- src/HumanBytes/ByteSize.cs
+++ src/HumanBytes/ByteSize.cs
@@ -5,18 +5,13 @@ namespace HumanBytes
     /// <summary>
     /// Represents a size in bytes.
     /// </summary>
-    public struct ByteSize
+    public readonly struct ByteSize : IEquatable<ByteSize>, IComparable<ByteSize>
     {
-        private readonly long _value;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="ByteSize"/> struct.
         /// </summary>
         /// <param name="value">The size in bytes.</param>
-        public ByteSize(long value)
-        {
-            _value = value;
-        }
+        public ByteSize(long value) => Value = value;
 
         /// <summary>
         /// Gets the size in bytes.
@@ -24,10 +19,7 @@ namespace HumanBytes
         /// <value>
         /// The size in bytes.
         /// </value>
-        public long Value
-        {
-            get { return _value; }
-        }
+        public long Value { get; }
 
         /// <summary>
         /// Returns a <see cref="System.String" /> that represents this instance.
@@ -40,7 +32,7 @@ namespace HumanBytes
         /// </remarks>
         public override string ToString()
         {
-            return ByteSizeFormatter.Default.Format(_value);
+            return ByteSizeFormatter.Default.Format(Value);
         }
 
         /// <summary>
@@ -53,9 +45,37 @@ namespace HumanBytes
         /// <exception cref="System.ArgumentNullException"><c>formatter</c> is null.</exception>
         public string ToString(ByteSizeFormatter formatter)
         {
-            if (formatter == null) throw new ArgumentNullException("formatter");
-            return formatter.Format(_value);
+            if (formatter == null) throw new ArgumentNullException(nameof(formatter));
+            return formatter.Format(Value);
         }
+
+        /// <inheritdoc />
+        public bool Equals(ByteSize other) => Value == other.Value;
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is ByteSize other && Equals(other);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => Value.GetHashCode();
+
+        /// <summary>
+        /// Determines whether two specified instances of <see cref="ByteSize"/> are equal.
+        /// </summary>
+        /// <param name="left">The first <see cref="ByteSize"/> instance to compare.</param>
+        /// <param name="right">The second <see cref="ByteSize"/> instance to compare.</param>
+        /// <returns></returns>
+        public static bool operator ==(ByteSize left, ByteSize right) => left.Equals(right);
+
+        /// <summary>
+        /// Determines whether two specified instances of <see cref="ByteSize"/> are not equal.
+        /// </summary>
+        /// <param name="left">The first <see cref="ByteSize"/> instance to compare.</param>
+        /// <param name="right">The second <see cref="ByteSize"/> instance to compare.</param>
+        /// <returns></returns>
+        public static bool operator !=(ByteSize left, ByteSize right) => !left.Equals(right);
+
+        /// <inheritdoc />
+        public int CompareTo(ByteSize other) => Value.CompareTo(other.Value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Int64"/> to <see cref="ByteSize"/>.
@@ -64,10 +84,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(long value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(long value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.UInt64"/> to <see cref="ByteSize"/>.
@@ -76,10 +93,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(ulong value)
-        {
-            return new ByteSize(checked((long)value));
-        }
+        public static implicit operator ByteSize(ulong value) => new ByteSize(checked((long)value));
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Int32"/> to <see cref="ByteSize"/>.
@@ -88,10 +102,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(int value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(int value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.UInt32"/> to <see cref="ByteSize"/>.
@@ -100,10 +111,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(uint value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(uint value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Int16"/> to <see cref="ByteSize"/>.
@@ -112,10 +120,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(short value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(short value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.UInt16"/> to <see cref="ByteSize"/>.
@@ -124,10 +129,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(ushort value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(ushort value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Byte"/> to <see cref="ByteSize"/>.
@@ -136,10 +138,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(byte value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(byte value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.SByte"/> to <see cref="ByteSize"/>.
@@ -148,10 +147,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(sbyte value)
-        {
-            return new ByteSize(value);
-        }
+        public static implicit operator ByteSize(sbyte value) => new ByteSize(value);
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Double"/> to <see cref="ByteSize"/>.
@@ -160,10 +156,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(double value)
-        {
-            return new ByteSize(checked((long) value));
-        }
+        public static implicit operator ByteSize(double value) => new ByteSize(checked((long) value));
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Single"/> to <see cref="ByteSize"/>.
@@ -172,10 +165,7 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(float value)
-        {
-            return new ByteSize(checked((long) value));
-        }
+        public static implicit operator ByteSize(float value) => new ByteSize(checked((long) value));
 
         /// <summary>
         /// Performs an implicit conversion from <see cref="System.Decimal"/> to <see cref="ByteSize"/>.
@@ -184,9 +174,6 @@ namespace HumanBytes
         /// <returns>
         /// The result of the conversion.
         /// </returns>
-        public static implicit operator ByteSize(decimal value)
-        {
-            return new ByteSize(checked((long) value));
-        }
+        public static implicit operator ByteSize(decimal value) => new ByteSize((long) value);
     }
 }
---
+++ tests/HumanBytes.Tests/ByteSizeTests.cs
@@ -0,0 +1,108 @@
+﻿using Xunit;
+
+namespace HumanBytes.Tests
+{
+    public class ByteSizeTests
+    {
+        #region Equals(object)
+
+        [Fact]
+        public void Equals_with_object_returns_true_if_parameter_is_ByteSize_with_same_value()
+        {
+            var left = new ByteSize(256);
+            object right = new ByteSize(256);
+            Assert.True(left.Equals(right));
+        }
+
+        [Fact]
+        public void Equals_with_object_returns_false_if_parameter_is_ByteSize_with_different_value()
+        {
+            var left = new ByteSize(256);
+            object right = new ByteSize(128);
+            Assert.False(left.Equals(right));
+        }
+
+        [Fact]
+        public void Equals_with_object_returns_false_if_parameter_is_not_ByteSize()
+        {
+            var left = new ByteSize(256);
+            var right = "hello";
+            Assert.False(left.Equals(right));
+        }
+
+        #endregion
+
+        #region Equals(ByteSize)
+
+        [Fact]
+        public void Equals_with_ByteSize_returns_true_if_parameter_has_same_value()
+        {
+            var left = new ByteSize(256);
+            var right = new ByteSize(256);
+            Assert.True(left.Equals(right));
+        }
+
+        [Fact]
+        public void Equals_with_ByteSize_returns_false_if_parameter_has_different_value()
+        {
+            var left = new ByteSize(256);
+            var right = new ByteSize(128);
+            Assert.False(left.Equals(right));
+        }
+
+        #endregion
+
+        #region Operator ==
+
+        [Fact]
+        public void Equality_operator_returns_true_if_parameter_has_same_value()
+        {
+            var left = new ByteSize(256);
+            var right = new ByteSize(256);
+            Assert.True(left == right);
+        }
+
+        [Fact]
+        public void Equality_operator_returns_false_if_parameter_has_different_value()
+        {
+            var left = new ByteSize(256);
+            var right = new ByteSize(128);
+            Assert.False(left == right);
+        }
+
+        #endregion
+
+        #region Operator !=
+
+        [Fact]
+        public void Inequality_operator_returns_false_if_parameter_has_same_value()
+        {
+            var left = new ByteSize(256);
+            var right = new ByteSize(256);
+            Assert.False(left != right);
+        }
+
+        [Fact]
+        public void Inequality_operator_returns_true_if_parameter_has_different_value()
+        {
+            var left = new ByteSize(256);
+            var right = new ByteSize(128);
+            Assert.True(left != right);
+        }
+
+        #endregion
+
+        #region CompareTo
+
+        [Theory]
+        [InlineData(128, 128, 0)]
+        [InlineData(128, 256, -1)]
+        [InlineData(256, 128, 1)]
+        public void CompareTo_returns_value_indicating_which_is_greater(int left, int right, int expected)
+        {
+            Assert.Equal(expected, left.Bytes().CompareTo(right.Bytes()));
+        }
+
+        #endregion
+    }
+}
